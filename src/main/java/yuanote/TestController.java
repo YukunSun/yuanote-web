6ce48d93091f72d3f63df68933485ce077c95528
YukunSun <sun-yukun@qq.com>
2016-04-07 14:24:56 +0800

    项目搭建

---
+++ src/main/java/yuanote/TestController.java
@@ -0,0 +1,9 @@
+package yuanote;
+
+public class TestController {
+
+	public static void main(String[] args) {
+		System.out.println("TestController");
+	}
+
+}
---
+++ src/test/java/yuanote/testHello.java
@@ -0,0 +1,15 @@
+package yuanote;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testHello {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		System.out.println("test");
+	}
+
+}
