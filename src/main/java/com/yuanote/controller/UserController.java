7ea298e537855a914080addbba98df8c5e734529
YukunSun <sun-yukun@qq.com>
2016-04-18 12:41:41 +0800

    登录过滤

--- src/main/java/com/yuanote/controller/UserController.java
+++ src/main/java/com/yuanote/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.yuanote.domain.User;
 import com.yuanote.service.IUserService;
@@ -16,11 +17,11 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 	
-	@RequestMapping("/getUser")
+	@RequestMapping(value="/getUser", method=RequestMethod.GET)  
 	public String test(HttpServletRequest request, Model model) {
 //		User user = this.userService.getUserById(Long.parseLong(request.getParameter("id")));
 //		String userName = user.getName();
 //		model.addAttribute("user", userName);
-		return "redirect:/login";
+		return "dd";
 	}
 }
