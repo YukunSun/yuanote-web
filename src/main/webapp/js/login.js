7a8869e165cf8731ee7f62bf5feac748619ee429
yukunsun <sun-yukun@qq.com>
2016-05-06 09:05:16 +0800

    fix: ajaxForm提交问题

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -13,23 +13,29 @@ function login() {
 	if (userName == "" || pwd == "") {
 		return false;
 	}
-	$.ajax({
-		type : "POST",
-		url : basePath + "user/login",
-		dataType:"json",
-		data : {
-			userName : userName,
-			password : pwd
-		},
-		success : function(result) {
-			alert(result.message);
-			if (result.status == 0) {
-			}
-		},
-		error : function(xhr, status, error) {
-			alert("请求失败！");
-		}
+	var tmp = $('#login_form').formSerialize();
+	//使用的是ajaxForm插件
+	$('#login_form').ajaxForm(function(result) {
+		alert(result.message);// 弹出ajax请求后的回调结果
 	});
+
+	// $.ajax({
+	// type : "post",
+	// url : basePath + "user/login",
+	// dataType:"json",
+	// data : {
+	// userName : userName,
+	// password : pwd
+	// },
+	// success : function(result) {
+	// alert(result.message);
+	// if (result.status == 0) {
+	// }
+	// },
+	// error : function(xhr, status, error) {
+	// alert("请求失败！");
+	// }
+	// });
 }
 
 function register() {
@@ -40,14 +46,14 @@ function register() {
 		password : pwd
 	};
 	$.ajax({
-		type : "POST",
+		type : "post",
 		url : basePath + "user/register",
-		dataType : "JSON",
+		dataType : "json",
 		data : user,
 		success : function(result) {
-//			if (result.status == 0) {
-//				alert(result.message);
-//			}
+			// if (result.status == 0) {
+			// alert(result.message);
+			// }
 		},
 		error : function(xhr, status, error) {
 			alert("请求失败！");
